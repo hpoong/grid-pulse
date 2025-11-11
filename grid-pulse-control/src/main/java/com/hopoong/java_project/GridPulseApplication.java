975bf75186ae7aead4886a5411f6322b48dbcf17
ho_poong <hopoong23@gmail.com>
2025-11-11 11:22:13 +0900

    feat(grid-pulse-control): 모듈 분리

--- grid-pulse-control/src/main/java/com/hopoong/java_project/GridPulseApplication.java
+++ grid-pulse-control/src/main/java/com/hopoong/java_project/GridPulseApplication.java
@@ -2,12 +2,12 @@ package com.hopoong.java_project;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class GridPulseApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(GridPulseApplication.class, args);
 	}
-
 }
